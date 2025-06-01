132e11e803b07a80a63adf2f65fe85e79ae75354
fira-pro <addisdebebe79@gmail.com>
2025-06-01 11:59:06 +0300

    add js config to fix mui auto import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { Button, Container } from "@mui/material";
+
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Hello world
+      <Button>Click Me</Button>
+    </div>
+  );
 }
