3dc1928effa9ab0f94e1930209fb7d58c47d2693
fira-pro <addisdebebe79@gmail.com>
2025-06-25 12:06:55 +0300

    -fix theme toggling to work when the selected mode is system. -add context menu option for the theme toggle to let user choose system mode

--- src/components/layout/AppBarContent.jsx
+++ src/components/layout/AppBarContent.jsx
@@ -6,12 +6,15 @@ import {
 } from "@mui/icons-material";
 import {
   IconButton,
+  Menu,
+  MenuItem,
   Toolbar,
   Tooltip,
   Typography,
   useColorScheme,
 } from "@mui/material";
 import { useLayout } from "./LayoutContext";
+import { useState } from "react";
 
 // const Container = styled("div")({
 //   containerType: "inline-size",
@@ -20,10 +23,43 @@ import { useLayout } from "./LayoutContext";
 
 export default function AppBarContent() {
   const { isDrawerOpen, setIsDrawerOpen } = useLayout();
-  const { mode, setMode } = useColorScheme();
+  const { mode, setMode, systemMode } = useColorScheme();
+  const effectiveMode =
+    mode === "system" ? systemMode : mode;
+
+  const [contextMenu, setContextMenu] = useState(null);
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX + 2,
+            mouseY: event.clientY + 20,
+          }
+        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+          // Other native context menus might behave different.
+          // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+          null
+    );
+
+    // Prevent text selection lost after opening the context menu on Safari and Firefox
+    const selection = document.getSelection();
+    if (selection && selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+
+      setTimeout(() => {
+        selection.addRange(range);
+      });
+    }
+  };
+
+  const handleClose = () => {
+    setContextMenu(null);
+  };
 
   return (
-    // <Container>
     <Toolbar>
       {!isDrawerOpen && (
         <Tooltip
@@ -67,23 +103,66 @@ export default function AppBarContent() {
         Fira
       </Typography>
       <Tooltip
-        title="Toggle theme"
+        title="Toggle theme, right click for options"
         placement="auto"
         disableInteractive
+        onContextMenu={handleContextMenu}
       >
         <IconButton
           size="large"
           aria-label="Toggle theme"
           onClick={() =>
-            setMode(mode === "light" ? "dark" : "light")
+            setMode(
+              effectiveMode === "light" ? "dark" : "light"
+            )
           }
         >
-          {mode === "dark" && <LightMode />}
-          {mode === "light" && <DarkMode />}
+          {effectiveMode === "dark" && <LightMode />}
+          {effectiveMode === "light" && <DarkMode />}
         </IconButton>
       </Tooltip>
-    </Toolbar>
 
-    // </Container>
+      <Menu
+        open={contextMenu !== null}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? {
+                top: contextMenu.mouseY,
+                left: contextMenu.mouseX,
+              }
+            : undefined
+        }
+      >
+        <MenuItem
+          selected={mode === "system"}
+          onClick={() => {
+            handleClose();
+            setMode("system");
+          }}
+        >
+          System
+        </MenuItem>
+        <MenuItem
+          selected={mode === "light"}
+          onClick={() => {
+            handleClose();
+            setMode("light");
+          }}
+        >
+          Light
+        </MenuItem>
+        <MenuItem
+          selected={mode === "dark"}
+          onClick={() => {
+            handleClose();
+            setMode("dark");
+          }}
+        >
+          Dark
+        </MenuItem>
+      </Menu>
+    </Toolbar>
   );
 }
