9038b851a7cdaadbd4799387f7b9a48e5fbc8624
fira-pro <addisdebebe79@gmail.com>
2025-06-26 18:17:14 +0300

    create ChatWindow component

---
+++ src/components/chat/ChatWindow.jsx
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+export default function ChatWindow() {
+  return (
+    <Box
+      maxWidth="sm"
+      sx={{
+        border: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "start",
+        alignItems: "flex-end",
+        // padding: 2,
+      }}
+    >
+      hi
+    </Box>
+  );
+}
