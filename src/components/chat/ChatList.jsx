ec59f22b07751344e11579aea2558e98453aa667
fira-pro <addisdebebe79@gmail.com>
2025-07-11 11:35:02 +0300

    - modify to use the new store

--- src/components/chat/ChatList.jsx
+++ src/components/chat/ChatList.jsx
@@ -1,16 +1,57 @@
-import React from "react";
-import useChatStore from "src/store";
 import ChatMessage from "./ChatMessage";
-
+import usePortfolioStore from "src/store";
+import Box from "@mui/material/Box";
+import IntroHeader from "./IntroHeader";
+import Suggestions from "./Suggestions";
+import DrawerHeader from "../layout/DrawerHeader";
+import Container from "@mui/material/Container";
 const ChatList = () => {
-  const chatList = useChatStore((s) => s.chatList);
+  const { sections, streamedSections, currentStreamingId } =
+    usePortfolioStore();
+
+  const isStreaming = currentStreamingId !== null;
+  const isJustStarted = streamedSections.length === 0;
+
+  // ! Needs review
+  // useEffect(() => {
+  //   if (isStreaming && showScrollButton) scrollToBottom();
+  // }, [isStreaming, showScrollButton, scrollToBottom]);
 
   return (
-    <>
-      {chatList.map((chat) => (
-        <ChatMessage key={chat.id} {...chat} />
-      ))}
-    </>
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      {isJustStarted && <IntroHeader />}
+      <Suggestions />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          alignItems: "center",
+          width: "100%",
+          p: 2,
+        }}
+      >
+        <p>{isStreaming ? "streaming" : "not streaming"}</p>
+        {streamedSections.map((sectionId) => {
+          const section = sections.find(
+            (s) => s.id === sectionId
+          );
+          return (
+            <ChatMessage
+              key={sectionId}
+              section={section}
+            />
+          );
+        })}
+      </Box>
+    </Container>
   );
 };
 
