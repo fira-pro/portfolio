1228fcf68784ebaacc7fe335fb802cebb94691bc
fira-pro <addisdebebe79@gmail.com>
2025-07-13 16:17:56 +0300

    clean up unused import

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,6 +1,4 @@
-import Container from "@mui/material/Container";
 import ChatWindow from "../chat/ChatWindow";
-import DrawerHeader from "./DrawerHeader";
 export default function MainContent() {
   return <ChatWindow />;
 }
