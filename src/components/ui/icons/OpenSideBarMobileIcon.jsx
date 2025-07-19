ccd13903a9782548fea03f87e1ddbf4e2031fbfe
fira-pro <addisdebebe79@gmail.com>
2025-07-19 14:44:20 +0300

    create custom icon components

---
+++ src/components/ui/icons/OpenSideBarMobileIcon.jsx
@@ -0,0 +1,30 @@
+import SvgIcon from '@mui/material/SvgIcon';
+export default function OpenSideBarMobileIcon() {
+  return (
+    <SvgIcon>
+      <svg
+        width="64px"
+        height="64px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <path
+            d="M4 6H20M4 12H14M4 18H9"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </g>
+      </svg>
+    </SvgIcon>
+  );
+}
