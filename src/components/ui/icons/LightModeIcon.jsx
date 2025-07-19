556d7659619633aecc54f5f1b0c50f78802b9315
fira-pro <addisdebebe79@gmail.com>
2025-07-19 19:37:01 +0300

    add more custom icons

---
+++ src/components/ui/icons/LightModeIcon.jsx
@@ -0,0 +1,22 @@
+import SvgIcon from "@mui/material/SvgIcon";
+export default function LightModeIcon() {
+  return (
+    <SvgIcon>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+        fill="none"
+        viewBox="-2.4 -2.4 28.8 28.8"
+      >
+        <g stroke="currentColor" strokeWidth="2">
+          <circle cx="12" cy="12" r="6"></circle>
+          <path
+            strokeLinecap="round"
+            d="M12 2v1M12 21v1M22 12h-1M3 12H2M19.07 4.93l-.392.393M5.322 18.678l-.393.393M19.07 19.07l-.392-.393M5.322 5.322l-.393-.393"
+          ></path>
+        </g>
+      </svg>
+    </SvgIcon>
+  );
+}
